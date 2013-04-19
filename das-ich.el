6e0fcd0f347536cb86fd5c49ce9a655f4cdc5bf2
Jaime Fournier <jaimef@linbsd.org>
2013-04-18 19:21:28 -0700

    initial checkin

---
+++ das-ich.el
@@ -0,0 +1,36 @@
+;;; dasich.el --- Run Go-lang code in Elisp.
+
+;;; -*- lexical-binding: t -*-
+
+;; Copyright (C) 2013  Jaime Fournier <jaimef@linbsd.org>
+
+;; Author: Jaime Fournier <jaimef@linbsd.org>
+;; Keywords: Thinatra
+;; Version: 0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Why?
+;; The question is How. The question is ALWAYS how. -Mitnick
+
+
+;;; Code:
+
+;; Example Functions that handle calls to rest endpoints can be found in examples.el
+(defun go-run
+  (message "Am in gorun!")
+
+  )
